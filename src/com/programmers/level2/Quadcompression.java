06e4775754f17f438cf027bf45ce282968644e47
guhan <guhan589@naver.com>
2021-03-19 13:35:35 +0900

    쿼드압축 후 개수세기 미완성

---
+++ src/com/programmers/level2/Quadcompression.java
@@ -0,0 +1,22 @@
+package com.programmers.level2;
+
+public class Quadcompression {
+
+    /**
+     * 쿼드압축 후 개수 세기(Level2) 미완성
+     * **/
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}};
+        int[] result = solution(arr);
+        for (int i : result) {
+            System.out.println("i = " + i);
+        }
+    }
+    static int[] solution(int[][] arr) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+}
