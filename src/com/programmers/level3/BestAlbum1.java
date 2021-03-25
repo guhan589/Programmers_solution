7bfe0cc3e1db11dc8f063f08d76db1df3b6dfed4
guhan <guhan589@naver.com>
2021-03-25 22:11:01 +0900

    베스트앨벌

--- src/com/programmers/level3/BestAlbum1.java
+++ src/com/programmers/level3/BestAlbum1.java
@@ -29,55 +29,77 @@ public class BestAlbum1 {
         Map<Integer, String> gmap = new HashMap<>(); //index - 장르명
         Map<Integer, Integer> pmap = new HashMap<>();//index - 플레이시간
         Set<String> gset = new HashSet<>(); // 중복되는 장르 삭제
-        
+
 
         for (int i = 0; i < genres.length; i++) {
-            gmap.put(i, genres[i]); //장르명
             gset.add(genres[i]);// 중복되는 장르 삭제
+            gmap.put(i, genres[i]); //장르명
             pmap.put(i, plays[i]); // 플레이시간 
         }
-        Map<String, Integer> totalPlayTime = new HashMap<>();
 
-        for (int i = 0; i < gmap.size(); i++) {
-            int temp = 0;
-            if (totalPlayTime.containsKey(gmap.get(i))) {
-                temp += totalPlayTime.get(gmap.get(i)) + pmap.get(i);
-                totalPlayTime.put(gmap.get(i), temp);
-            }else
-                totalPlayTime.put(gmap.get(i), pmap.get(i));
+
+        
+
+        Map<Integer, String> totalPlayTime = new HashMap<>(); // 장르별 재생횟수(총재생시간, 장르)
+        for (String o : gset) {
+            int total = 0; //총재생시간 변수
+            for (int i = 0; i < gmap.size(); i++) {
+                if(gmap.get(i).equals(o))
+                    total += pmap.get(i); //총 재생시간
+            }
+            totalPlayTime.put(total, o);//횟수와 재생시간
         }
 
 
-    /*    for (String s : totalPlayTime.keySet()) {
-            System.out.println("map.get(s) = " + s);
-            System.out.println("map.get(s) = " + totalPlayTime.get(s));
+        TreeMap treeMap = new TreeMap(totalPlayTime);
+        String[] sorttemp = new String[gset.size()];// hashset사이즈 만큼 배열 선언
 
-        }*/
-        /*for (int i = 0; i < genres.length; i++) {
-            list.add(new Musicinformation(genres[i], plays[i]));
-        }*/
+        int index = 0;
+        for (Object o : treeMap.keySet()) {
+            sorttemp[index++] = treeMap.get(o).toString(); //많이 재생된 시간별로 정렬
+        }
+        //treeMap.get(o).toString(); = 1450
+        //treeMap.get(o).toString(); = 3100
 
-        /*Collections.sort(list, new Comparator<Musicinformation>() {
-            @Override
-            public int compare(Musicinformation o1, Musicinformation o2) {
-                if(o1.genres.equals(o2.genres))
-                    return 0;
-                else if(o1.)
+        List<Integer> playtimeList = new ArrayList<>();
+        for (int i = sorttemp.length - 1; i > -1; i--) {
+            int cnt = 0;
+            for (int key : gmap.keySet()) {
+                System.out.println("key = " + key);
+                if (sorttemp[i].equals(gmap.get(key))) {
+                    cnt++;
+                }
             }
-        });*/
-//        map = new HashMap<>();
 
 
+            int[] temp = new int[cnt];
+            int count = 0;
+
+            for (int o : gmap.keySet()) {
+                if (sorttemp[i].equals(gmap.get(o))) {
+                    temp[count++] = pmap.get(o);
+                }
+            }
+
+
+        }
         return answer;
     }
-    static class Musicinformation{
+  
+    /*static class Musicinformation implements Comparable<Musicinformation>{
 
+        int index;
         String genres;
         int plays;
 
-        public Musicinformation(String genres, int plays) {
+        public Musicinformation(int index, String genres, int plays) {
+            this.index = index;
             this.genres = genres;
             this.plays = plays;
+        }
+
+        public int getIndex() {
+            return index;
         }
 
         public String getGenres() {
@@ -87,5 +109,16 @@ public class BestAlbum1 {
         public int getPlays() {
             return plays;
         }
-    }
+
+        @Override
+        public int compareTo(Musicinformation musicinformation) {
+            if(this.plays == musicinformation.plays)
+                return this.plays - musicinformation.plays;
+            else
+                return -(this.plays - musicinformation.plays);
+        }
+
+        //장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+
+    }*/
 }
--- src/com/programmers/level3/BestAlbum2.java
+++ src/com/programmers/level3/BestAlbum2.java
@@ -7,11 +7,14 @@ public class BestAlbum2 {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
         int[] plyas = {500, 600, 150, 800, 2500};
         int[] result = solution(genres, plyas);
-
+        for (int i : result) {
+            System.out.println(i);
+        }
     }
     public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
 
+        int[][] arr = new int[(int) Arrays.stream(genres).distinct().count()][2];
         /**
          * 제약조건
          * 1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
@@ -19,62 +22,61 @@ public class BestAlbum2 {
          * 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
          * */
 
-        int distict = (int) Arrays.stream(genres).distinct().count();
-        List<Musicinformation> musiclist = new ArrayList<>();
 
-        for (int i = 0; i < genres.length; i++)
-            musiclist.add(new Musicinformation(i,genres[i], plays[i]));
+        List<Musicinformation> list = new ArrayList<>();
+        Map<String, Integer> gmap = new HashMap<>(); //장르 - 플레이시간
+        Map<String, Integer> cntmap = new HashMap<>(); //장르 - 카운트
+        List<Integer> resultlist = new ArrayList<>(); // 결과 id값
 
+        for (int i = 0; i < genres.length; i++) {
+            list.add(new Musicinformation(i, genres[i], plays[i]));
+            if (gmap.containsKey(genres[i])) {
+                gmap.put(genres[i], gmap.get(genres[i]) + plays[i]);
+            }else
+                gmap.put(genres[i], plays[i]);
+        }
 
-
-//        같으면 0, 자신이 크면 양수, 자신이 작으면 음수를 반환
-        musiclist.sort(new Comparator<Musicinformation>() {
+        Collections.sort(list, new Comparator<Musicinformation>() {
             @Override
             public int compare(Musicinformation o1, Musicinformation o2) {
                 if(o1.getGenres().equals(o2.getGenres()))
-                    return o2.getPlays() - o1.getPlays();
-
-                return o1.getGenres().compareTo(o2.getGenres());
+                    return o1.compareTo(o2);
+                else
+                    return -(gmap.get(o1.getGenres()) - gmap.get(o2.getGenres()));
             }
         });
 
-//        Map<Integer, List<Musicinformation>> list = new HashMap<>();
-
-  /*      for (Musicinformation musicinformation : musiclist) {
+    /*    for (Musicinformation musicinformation : list) {
             System.out.println("musicinformation = " + musicinformation.getIndex());
-            System.out.println("musicinformation.getGenres() = " + musicinformation.getGenres());
-            System.out.println("musicinformation.getGenres() = " + musicinformation.getPlays());
-            System.out.println("=======================\n");
+            System.out.println("musicinformation = " + musicinformation.getGenres());
+            System.out.println("musicinformation = " + musicinformation.getPlays());
+            System.out.println("==================");
         }*/
 
-        Map<String, Integer> totaltime = new HashMap<>();
-        String temp = musiclist.get(0).getGenres();
-        totaltime.put(temp, musiclist.get(0).getPlays());
 
 
-        int cnt = 0;
-        for (int i = 1; i < musiclist.size(); i++) {
-            String str = musiclist.get(i).getGenres();
-            System.out.println("str = " + str);
-            if (temp.equals(str) && cnt <2) {
-//                System.out.println("totaltime.get(temp) = " + totaltime.get(temp));
-                totaltime.put(str,totaltime.get(temp)+musiclist.get(i).getPlays());
-                ++cnt;
+        for (Musicinformation entity : list) {
+            if (!cntmap.containsKey(entity.getGenres())) { // 카운트를 의미하는 map에 key가 없을경우
+                cntmap.put(entity.getGenres(), 1);// 장르 - 카운트 1
+                resultlist.add(entity.index);
             }else{
-                temp = str;
-                cnt = 0;
+                int cnt = cntmap.get(entity.getGenres());
+                if (cnt < 2) {
+                    cntmap.put(entity.genres, cnt + 1);
+                    resultlist.add(entity.index);
+                }
+
             }
         }
-
-        for (String s : totaltime.keySet()) {
-            System.out.println("s = " + s);
-            System.out.println("totaltime.get(s) = " + totaltime.get(s));
+        answer = new int[resultlist.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = resultlist.get(i);
         }
-
 
         return answer;
     }
-    static class Musicinformation{
+
+    static class Musicinformation implements Comparable<Musicinformation>{
 
         int index;
         String genres;
@@ -96,6 +98,15 @@ public class BestAlbum2 {
 
         public int getIndex() {
             return index;
+        }
+
+        @Override
+        public int compareTo(Musicinformation o) {
+            if(this.plays == o.plays)
+                return this.index - o.index;
+            else
+                return -(this.plays - o.plays);
+
         }
     }
 }
