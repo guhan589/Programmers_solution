87c8befeebb57a4debf8f1752587007dbd1ab08a
GuhwanKwon <guhan589@naver.com>
2021-04-19 17:11:13 +0900

    입국심사 level3(완성) 주석 처리

--- src/com/programmers/level3/Immigration.java
+++ src/com/programmers/level3/Immigration.java
@@ -20,22 +20,30 @@ public class Immigration {
 
         long start, end, mid, sum = 0;
         start = 1;
-        end = (long) n * times[times.length - 1];
+        end = (long) n * times[times.length - 1]; // 최대 심사시간
 
-        answer = end;
+//        answer = end; //최악의 상황을 고려
+
+        int count = 1;
         while (start <= end) {
-            mid = (start + end) / 2;
-
+            System.out.println("====" + count + "====");
+            ++count;
+            mid = (start + end) / 2; //중간 값
+            System.out.println("mid = " + mid);
             sum = 0;
             for (int i = 0; i < times.length; i++) {
+                // 반복문을 통해 각 심사자들의 소요시간에 따른 몇명의 사람들이 심사가 가능한지를 sum변수에 저장
                 sum += mid / times[i];
             }
-            if (n > sum) {
+            System.out.println("sum = " + sum);
+            if (n > sum) { // 심사해야할 사람이 심사가능한 수보다 많을 경우
                 start = mid + 1;
             } else {
                 end = mid - 1;
                 answer = mid;
             }
+            System.out.println("start = " + start);
+            System.out.println("end = " + end);
         }
         return answer;
     }
