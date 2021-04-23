cacb4ab5f32875c1cdc3178dedc0a6ff3fbe02ae
GuhwanKwon <guhan589@naver.com>
2021-04-23 17:59:47 +0900

    메뉴 리뉴얼 level2 (완성)

--- src/com/programmers/level2/Menu.java
+++ src/com/programmers/level2/Menu.java
@@ -1,99 +1,82 @@
 package com.programmers.level2;
 
+import java.util.*;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Optional;
-
-/**
- * 
- * 메뉴 리뉴얼(미완성)
- *
- *
- * **/
 public class Menu {
-
+    /**
+     * 메뉴리뉴얼 level2 (완성)
+     *
+     * */
+    static Map<String, Integer> map;
+    static List<String> list = new ArrayList<>();
     public static void main(String[] args) {
-        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-        String[] orders2 = {"XYZ", "XWY", "WXA"};
+        String[] orders1= {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+//        String[] orders1 = {"XYZ", "XWY", "WXA"};
         int[] course = {2, 3, 4};
 
-        String[] result = solution(orders2, course);
+        String[] result = solution(orders1, course);
         for (String s : result) {
             System.out.println("result = " + s);
         }
-
-
     }
-    public static String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        int min = course[0];
-        HashMap<Character, Integer> map = putValue(orders);
-        ArrayList<Character> deleteChList = new ArrayList<>(); //삭제할 문자 리스트
-        ArrayList<String> addChList = new ArrayList<>(); //유효한 문자 리스트
-        ArrayList<String> resultList = new ArrayList<>();//정상 문자 리스트
-        
-        for (Character ch : map.keySet()) {
 
-//            System.out.println("ch + = " + ch + ", ch.get():" + map.get(ch));
-            if(map.get(ch) > 1)
-                addChList.add(String.valueOf(ch));
-            else
-                deleteChList.add(ch);
-        }
-
-
-        for (Character value : deleteChList)
-            map.remove(value);
-
-
+    public static String[] solution(String[] orders, int[] coures) {
+        String[] answer;
         for (int i = 0; i < orders.length; i++) {
-            int time = 0;
-            for (int j = 0; j < addChList.size(); j++) {
-                if (orders[i].contains(addChList.get(j))) {
-                    if (time >= min) {
-                        resultList.add(orders[i]);
-                        break;
-                    }else
-                        time++;
+            char[] ch = orders[i].toCharArray();//String convert to char
+            Arrays.sort(ch);
+            for (int index = 0; index < ch.length - 1; index++) {// ch[] 배열
+                for (int co = 0; co < coures.length; co++) { // 주문 수
+                    dfs(ch, index, coures[co], String.valueOf(ch[index]));
                 }
-            }
 
+            }
         }
 
-        System.out.println("resultList.size() = " + resultList.size());
+        addMap(list); // list에 모든 값을 HashMap에 put하는 함수 호출
+
+
+        //key값을 기준으로 sort
+
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList,(o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+
+        List<String> resultList = new ArrayList<>();
+        for (int index = 0; index < coures.length; index++) {
+            int max = 0;
+
+            for (String s : keyList) {
+                if (map.get(s) >= 2 && s.length() == coures[index]) {
+                    if (map.get(s) >= max) {
+                        resultList.add(s);
+                        max = map.get(s);
+                    }
+                }
+
+            }
+        }
+
+        Collections.sort(resultList);
         answer = new String[resultList.size()];
         int i = 0;
         for (String s : resultList) {
             answer[i++] = s;
         }
-
-
-
         return answer;
     }
-    static HashMap putValue(String[] orders){
-        /**
-         * HashMap에 데이터 값이 넣기(메뉴, 개수)
-         * */
-        HashMap<Character, Integer> map = new HashMap<>();
 
-        for (String s : orders) {
-            char[] ch = s.toCharArray();
-
-            for (char c : ch) {
-                int value = map.getOrDefault(c, 0);
-                //값이 없을경우 defalut값을 받는다.
-                map.put(c, ++value);
-
-            }
+    static void addMap(List<String> list) {
+        map = new HashMap<>();
+        for (String s : list) {
+            map.put(s,map.getOrDefault(s, 0) + 1);
         }
-
-        return map;
     }
+    static void dfs(char[] ch, int index, int coure, String str) {
+        if (str.length() == coure)
+            list.add(str);
 
+        for (int i = index + 1; i < ch.length; i++)
+            dfs(ch,i,coure,str+ch[i]);
+
+    }
 }
-
-
