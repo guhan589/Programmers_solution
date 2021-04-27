cac53112d14fbd2460b6595a500dc94fc6b17dc4
GuhwanKwon <guhan589@naver.com>
2021-04-27 20:00:20 +0900

    소수 찾기 level2 (미완성)

--- src/com/programmers/level2/PrimenumberFind.java
+++ src/com/programmers/level2/PrimenumberFind.java
@@ -1,9 +1,15 @@
 package com.programmers.level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 소수 찾기 level 2 (미완성)
  * */
 public class PrimenumberFind {
+
+    static List<Integer> list = new ArrayList<>();
+    static int[] number;
     public static void main(String[] args) {
         String numbers = "17";
         int result = solution(numbers);
@@ -13,15 +19,40 @@ public class PrimenumberFind {
     public static int solution(String numbers) {
         int answer = 0;
         int i = 0;
-        char[] splitNumber = new char[numbers.length()];
-        boolean[] boolNumber = new boolean[numbers.length()];
+        char[] chnumber = String.valueOf(numbers).toCharArray();
+        /*for (char c : chnumber) {
+            System.out.println("c = " + c);
+        }*/
 
 
-        for( ;i<numbers.length();i++){
+        boolean b = modifyPrimenumber(70); // true: 소수o / false: 소수x
 
-        }
+        System.out.println("b = " + b);
+
 
         return answer;
+    }
+
+    static List<Integer> makeNumber(char[] number) {
+
+        return list;
+    }
+    static boolean modifyPrimenumber(int number){
+        boolean result = false; //defalut = false
+        int count = 0;
+        double sqrt = Math.sqrt(number); //제곱근 까지 loop를 돌림
+        for (int i = 1; i < sqrt; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+
+            if(count>2)
+                return false;
+        }
+
+        result = true;
+//        else result = true;//소수
+        return result;
     }
 
 }
