df13de376bf56dfcc5aa5ce8a8a20ffaf25f53d7
guhan589 <guhan589@naver.com>
2021-12-26 22:12:23 +0900

    2개이하로 다른 비트 (미완성)

---
+++ src/com/programmers/level2/OtherBit.java
@@ -0,0 +1,15 @@
+package com.programmers.level2;
+
+public class OtherBit {
+    public static void main(String[] args) {
+        long[] numbers = {2, 7};
+        long[] result = solution(numbers);
+        for (long l : result) {
+            System.out.println("l = " + l);
+        }
+    }
+    public static long[] solution(long[] numbers) {
+        long[] answer = {};
+        return answer;
+    }
+}
