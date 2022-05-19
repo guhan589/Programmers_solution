462dc33113ab80a1fdd5110dcc7480536be6670a
GuhwanKwon <guhan589@naver.com>
2022-05-19 18:59:01 +0900

    N의제곱

---
+++ src/com/programmers/level2/SquareN.java
@@ -0,0 +1,16 @@
+package com.programmers.level2;
+
+public class SquareN {
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static int[] solution(int n, long left, long right)
+    {
+        int[] answer = new int[0];
+
+        return answer;
+    }
+}
+
