47893a00cbe7b62a84e3e39ae34c3ac8af9e56a7
GuhwanKwon <guhan589@naver.com>
2022-07-17 17:10:35 +0900

    temp

--- src/com/programmers/level2/OtherTwoBit.java
+++ src/com/programmers/level2/OtherTwoBit.java
@@ -13,8 +13,29 @@ public class OtherTwoBit {
     }
 
     public static long[] solution(long[] numbers) {
-        long[] answer = {};
-        long number = numbers[0];
+        long[] answer = new long[numbers.length]; //return 할 배열 사이즈 선언
+
+        for (int i = 0; i < numbers.length; i++) {
+            String BinaryCode = Long.toBinaryString(numbers[i]);
+            //long값을 바이너리 코드 값으로 변경
+            System.out.println("BinaryCode = " + BinaryCode);
+
+            if (numbers[i] % 2 == 0) {
+                answer[i] += 1; // bit는 2의 제곱승으로 표현하기에 사실상 마지막 비트의 0값을
+                // 바꾼 값이 2개 이하로 다른 bit값이다.
+
+            } else {
+                int lastValue = BinaryCode.lastIndexOf("0");
+                int firstValue = BinaryCode.lastIndexOf("1", lastValue);
+                System.out.println("lastValue = " + lastValue);
+                System.out.println("firstValue = " + firstValue);
+
+                if (lastValue == -1) { //Binary 코드 값
+
+                }
+            }
+            System.out.println();
+        }
 
         return answer;
     }
