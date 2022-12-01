415e297f7675358e25b3e4f9866ecf04cc716756
GuhwanKwon <guhan589@naver.com>
2022-12-01 19:02:45 +0900

    멀리뛰기

---
+++ src/com/programmers/level2/ContinueSum.java
@@ -0,0 +1,26 @@
+package com.programmers.level2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ContinueSum {
+    public static void main(String[] args) {
+        int[] elements = {7, 8, 1, 1, 4};
+        int solution = solution(elements);
+
+
+
+    }
+    public static int solution(int[] elements) {
+
+        for (int i = 0; i < elements.length; i++) {
+
+        }
+
+        return 0;
+    }
+
+}
--- src/com/programmers/level2/Jumpmove.java
+++ src/com/programmers/level2/Jumpmove.java
@@ -2,23 +2,17 @@ package com.programmers.level2;
 
 public class Jumpmove {
     public static void main(String[] args){
-        int n = 6;
-        int result = solution(n);
+        int n = 4;
+        long result = solution(n);
         System.out.println("result-->"+result);
     }
-    public static int solution(int n){
-        int ans = 0;
-
-        while(n!=0){
-            if(n%2==0){
-                n = n /2;
-            }else{
-                n--;
-                ans++;
-            }
+    public static long solution(int n){
+        long[] dpTree = new long[2001];
+        dpTree[1]=1;
+        dpTree[2]=2;
+        for(int i=3;i<=2000;i++){
+            dpTree[i] = (dpTree[i-1]+dpTree[i-2])%1234567;
         }
-
-        return ans;
+        return dpTree[n]%1234567;
     }
-
 }
