7a592e4aee523fef35047dfbb6d38d6548030b81
guhan589 <guhan589@naver.com>
2022-12-06 23:41:56 +0900

    기사단원의 무기

--- src/com/programmers/level1/Weapon.java
+++ src/com/programmers/level1/Weapon.java
@@ -1,5 +1,8 @@
 package com.programmers.level1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Weapon {
 
     public static void main(String[] args) {
@@ -10,33 +13,25 @@ public class Weapon {
     public static int solution(int number, int limit, int power) {
         int answer = 0;
 
-        answer = 2;
 
-        for (int i = 2; i <= number; i++) {
-            calDiv(i)
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+//            System.out.print("i = " + i+" ");
+
+            for (int j = 1; j * j <= i; j++) {
+//                System.out.println("j = " + j);
+                if (j * j == i) count++;
+                else if (i % j == 0) count += 2;
+
+            }
+
+            answer += count > limit ? power : count;
+
         }
         return answer;
     }
 
-    public static int calDiv(int num) {
-        int agx = 0;
-        int answer = 0;
-        if (num % 2 == 0) {//짝수
-            agx = num / 2;
-            if (agx == 1)
-                return 0;
 
-        }else{
-            if (num / 3 == 0) {
-                agx = num / 3;
-            }else
-                return 1;
-        }
-        for (int i = 2; i <= agx; i++) {
-
-        }
-        return 0;
-    }
 
 
 }
